5201060f9688fe2520bb79c3596298ae5b43682f
codebadolo <codewithbadolo@gmail.com>
2024-09-09 23:24:39 +0000

    jsx concepts

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,18 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>how are you</Text>
-    </View>
-  );
-}
+import Hello from '../components/Hello';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 're',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return <Hello /> ;
+};
+
+
+function Greet() { 
+    return <Text> am reading to code </Text> ;
+}
---
+++ src/components/Hello.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native";
+
+
+const  Hello = () => {
+    return <Text> Good moring to the community </Text> ;
+};
+
+
+export default Hello;
